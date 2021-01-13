fca800558555bd8e700cf1615753c3f36b9b1bff
Ozgewn <guisodrogadicto289@gmail.com>
2021-01-13 09:46:13 +0100

    Main acabado

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		
+		Survivor survi=new Survivor("Botiquín");
+		survi.mostrarEstadoSalud();
+		survi.mostrarVida();
+		survi.establecerEstadoSalud(7);
+		survi.establecerEstadoSalud(1);
+		survi.establecerObjeto("Caja de herramientas");
+		survi.mostrarEstadoSalud();
+		survi.establecerObjeto("asdasd");
 
-	}
+	} //FIN MAIN
 
-}
+} //FIN CLASE
