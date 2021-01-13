8bf711fbb608bd100f4088e7b324580bc33144e4
Ozgewn <guisodrogadicto289@gmail.com>
2021-01-13 09:20:34 +0100

    Atributos y estados de la clase

--- src/Survivor.java
+++ src/Survivor.java
@@ -1,7 +1,15 @@
 
 public class Survivor {
 	
-	public Survivor() {
+	//Atributos
+	int vidas, estadoSalud;
+	String objetoActual;
+	
+	public Survivor(String objetoActual) {
+		
+		vidas=3;
+		estadoSalud=2; //0=tumbado, 1=herido, 2=saludable
+		this.objetoActual=objetoActual;
 		
 	}
 
